f492d5d4162c01e865935414b3a9f3dd2837e2c4
viet <lhviet88@gmail.com>
2018-11-16 22:58:15 +0900

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,6 @@ import * as React from 'react';
 import logoSvg from '^/logo.svg';
 import styled, {Keyframes, keyframes, StyledComponent} from 'styled-components';
 
-class App extends React.Component {
-  public render(): React.ReactNode {
-    return (
-      <Root>
-        <Header>
-          <Logo src={logoSvg} alt='logo' />
-          <Title>Welcome to React</Title>
-        </Header>
-        <Intro>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </Intro>
-      </Root>
-    );
-  }
-}
-
 const LogoSpinAnimation: Keyframes = keyframes`
   from { transform: rotate(0deg); }
   to { transform: rotate(360deg); }
@@ -43,5 +27,21 @@ const Logo: StyledComponent<'img', {}> = styled.img`
 const Title: StyledComponent<'h1', {}> = styled.h1`
   font-size: 1.5em;
 `;
+
+class App extends React.Component {
+  public render(): React.ReactNode {
+    return (
+      <Root>
+        <Header>
+          <Logo src={logoSvg} alt='logo' />
+          <Title>Welcome to React</Title>
+        </Header>
+        <Intro>
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </Intro>
+      </Root>
+    );
+  }
+}
 
 export default App;
