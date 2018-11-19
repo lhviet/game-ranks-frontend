e7bc3f932b2dd376ddc6e54c8ad2cf6d6e19adc3
viet <lhviet88@gmail.com>
2018-11-19 13:53:12 +0900

    Update API host

--- src/store/duck/API.ts
+++ src/store/duck/API.ts
@@ -7,14 +7,14 @@ export const exhaustiveCheck: (x: never) => never = (x) => {
 export const protocol: string = 'http';
 export let serverHostname: string;
 if (!config.isBrowser || config.isNotProduction) {
-  serverHostname = 'localhost:3001';
+  serverHostname = 'ec2-13-125-251-167.ap-northeast-2.compute.amazonaws.com:3008';
 } else {
   switch (config.buildLevel) {
     case BuildLevel.DEVELOPMENT:
-      serverHostname = 'localhost:3001';
+      serverHostname = 'ec2-13-125-251-167.ap-northeast-2.compute.amazonaws.com:3008';
       break;
     case BuildLevel.PRODUCTION:
-      serverHostname = 'localhost:3001';
+      serverHostname = 'ec2-13-125-251-167.ap-northeast-2.compute.amazonaws.com:3008';
       break;
     default:
       exhaustiveCheck(config.buildLevel);
